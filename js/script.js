bb6f75662ea771731ef49b81fa2bfe627f032a88
NalaniRiddle <120990843+NalaniRiddle@users.noreply.github.com>
2023-02-07 15:53:04 -0500

    added more cities

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ function updateTime() {
     let newYorkDateElement = newYorkElement.querySelector(".date");
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTime = moment().tz("America/New_York");
-    
+
     newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
     newYorkTimeElement.innerHTML = newYorkTime.format(
       "h:mm:ss [<small>]A[</small>]"
-      );
-    }
+    );
+  }
 
   /// Paris
   let parisElement = document.querySelector("#paris");
@@ -18,9 +18,35 @@ function updateTime() {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
     let parisTime = moment().tz("Europe/Paris");
-    
+
     parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  /// Montreal
+  let montrealElement = document.querySelector("#montreal");
+  if (montrealElement) {
+    let montrealDateElement = montrealElement.querySelector(".date");
+    let montrealTimeElement = montrealElement.querySelector(".time");
+    let montrealTime = moment().tz("America/Montreal");
+
+    montrealDateElement.innerHTML = montrealTime.format("MMMM Do YYYY");
+    montrealTimeElement.innerHTML = montrealTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  /// Mexico City
+  let mexicoCityElement = document.querySelector("#mexico-city");
+  if (mexicoCityElement) {
+    let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+    let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+    let mexicoCityTime = moment().tz("America/Mexico_City");
+
+    mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do YYYY");
+    mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
   }
 }
 
